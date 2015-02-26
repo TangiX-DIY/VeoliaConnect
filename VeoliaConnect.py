697624e37d51e3e5d1ce7438b2e2de96d8094061
Christophe Braud <tangix.diy@free.fr>
2015-02-26 21:12:39 +0100

    add spreadsheet DL support

--- VeoliaConnect.py
+++ VeoliaConnect.py
@@ -7,6 +7,17 @@ Created on Sun Feb 15 11:01:40 2015
 """
 
 import argparse
+import cookielib,urllib2,urllib
+
+# Logger
+def Logger(pMessage):
+    file = open('/var/log/veoliaconnect.log', 'a')
+    file.write("%s/n" % pMessage)
+    file.close()
+
+# On active le support des cookies pour urllib2
+cookiejar = cookielib.CookieJar()
+urlOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar))
 
 # Parse arguments
 parser = argparse.ArgumentParser()
@@ -39,22 +50,75 @@ if not args.web and args.spreadsheet is None:
     
 # WebSite gathering
 if args.web:
-    #TODO
-    print 'TODO'
+    urlConnect = 'https://www.service-client.veoliaeau.fr/home.loginAction.do#inside-space'
+    urlConso1 = 'https://www.service-client.veoliaeau.fr/home/espace-client/votre-consommation.html'
+    urlConso2 = 'https://www.service-client.veoliaeau.fr/home/espace-client/votre-consommation.html?vueConso=historique'
+    urlXls = 'https://www.service-client.veoliaeau.fr/home/espace-client/votre-consommation.exportConsommationData.do?vueConso=historique'
+    urlDisconnect = 'https://www.service-client.veoliaeau.fr/logout'
+    
+    param = {'veolia_username' : args.login,
+             'veolia_password' : args.password,
+             'login' : 'OK'}
+    
+    # Connect to Veolia website
+    Logger('Connection au site Véolia Eau')
+    data = urllib.urlencode(param)
+    request = urllib2.Request(urlConnect, data)
+    request.add_header('Referer', 'https://www.service-client.veoliaeau.fr/home.html')
+    response = urlOpener.open(request)
+    Logger(response.getcode())
+    Logger(response.info())
+    #file = open('/tmp/home.html', 'w')
+    #file.write(response.read())
+    #file.close()
+    
+    # page votre consomation
+    #print 'Conso 1'
+    response = urlOpener.open(urlConso1)
+    #print response.getcode()
+    #print response.info()
+    #file = open('/tmp/conso1.html', 'w')
+    #file.write(response.read())
+    #file.close()
+    
+    #print 'Conso 2'
+    response = urlOpener.open(urlConso2)
+    #print response.getcode()
+    #print response.info()
+    #file = open('/tmp/conso2.html', 'w')
+    #file.write(response.read())
+    #file.close()
+    
+    # Download XLS file
+    #print 'Téléchargement du fichier'
+    response = urlOpener.open(urlXls)
+    #print response.getcode()
+    #print response.info()
+    content = response.read()
+    #file = open('/tmp/data.xls', 'w')
+    #file.write(content)
+    #file.close()
+    
+    #logout
+    #print "Déconnection du site Véolia Eau"
+    response = urlOpener.open(urlDisconnect)
+    #print response.getcode()
+    #print response.info()
 
 # Get external spreadsheet content
 if args.spreadsheet is not None:
     #TODO
-    print 'TODO'
+    print 'TODO read spreadsheet'
     
 # Store spreadsheet content
 if args.output is not None:
-    #TODO
-    print 'TODO'
+    file = open(args.output, 'w')
+    file.write(content)
+    file.close()
     
 # Parse spreadsheet contents
 if args.format != 'none':
     #TODO
-    print 'TODO'
+    print 'TODO parse data'
 
 
